44a591bc4ff901c6066474d8ae05107df592d7a9
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-07-06 18:36:34 +0300

    Topic Exchange

---
+++ RabbitMQ.Topic.Exchange.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://whlmzzhz:89x2AG2tGzVr0vc1y3F5MW9ndETSnHDP@rattlesnake.rmq.cloudamqp.com/whlmzzhz");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba: {i}");
+    Console.Write("Mesajın gönderileceği topic formatını belirtiniz");
+    string topic = Console.ReadLine();
+
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: message);
+}
+
+
+
+Console.Read();
